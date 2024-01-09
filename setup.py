91e853deb77285cbaf7e3917c2cc1c296ceabe6b
sadhiin <sanjidulislamsadhin1994@gmail.com>
2024-01-09 18:15:57 +0600

    project init

---
+++ setup.py
@@ -0,0 +1,39 @@
+import setuptools
+import subprocess
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+__version__ = "0.0.1"
+
+REPO_NAME = "Large-Class-Image-Classification"
+AUTHOOR_NAME = "Sadhin"
+SRC_REPO = "large-class-image-classification"
+AUTHOR_EMAIL = "sadhin.aiub.cse@gmail.com"
+
+setuptools.setup(
+    name=REPO_NAME,
+    version=__version__,
+    author=AUTHOOR_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A large class image classification with Deep learning",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/sadhiin/100-class-image-classifaction",
+    project_urls={
+        "Bug Tracker": f"https://github.com/sadhiin/100-class-image-classifaction/issues",
+    },
+    install_requires=[
+        'numpy==1.24.3',
+        'opencv-python==4.8.1.78',
+        'tensorflow==2.13',
+        'pillow==10.2.0',
+        'Flask==3.0.0',
+        'Flask-Cors==4.0.0',
+        'python-box==7.1.1',
+        'ensure==1.0.4'
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.9"
+)
---
+++ src/LCIC/__init__.py
@@ -0,0 +1,5 @@
+# https://www.kaggle.com/datasets/gpiosenka/sports-classification
+# https://www.kaggle.com/datasets/gpiosenka/headgear-image-classification
+# https://www.kaggle.com/datasets/gpiosenka/100-bird-species/data
+# https://www.kaggle.com/datasets/pavansanagapati/images-dataset
+# https://www.kaggle.com/datasets/gpiosenka/balls-image-classification/
